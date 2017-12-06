78415c88321a2ebf159c4737b64f369b79fce0d5
Kristoffer Jälén <kristoffer.jalen@tretton37.com>
2017-12-06 14:54:21 +0100

    Add solution problem 2

---
+++ Problem2/Problem2/P.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+class P
+{
+    static void Main()
+    {
+        var f = new int[99];
+        var i = f[0] = 1;
+        f[1] = 2;
+
+        while (f[i] < 4000000)
+            f[i + 1] = f[i] + f[i++ - 1];
+
+        System.Console.Write(f.Sum(x => x % 2 == 0 ? x : 0));
+    }
+}
+// 171 bytes minified
