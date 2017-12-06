8189897284b17bdf4524f925b8c785c366e96704
Kristoffer Jälén <kristoffer.jalen@tretton37.com>
2017-12-06 14:53:24 +0100

    Add solution problem 1

---
+++ Problem1/Problem1/C.cs
@@ -0,0 +1,11 @@
+﻿    class C
+    {
+        static void Main()
+        {
+            int i = 0, s = 0;
+            while (i++ < 999)
+                s += i % 3 == 0 || i % 5 == 0 ? i : 0;
+            System.Console.Write(s);
+        }
+    }
+// 105 bytes minified
